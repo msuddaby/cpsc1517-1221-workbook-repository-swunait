5d5f01423088d7b53a4b2c65342490822ca15026
Sam Wu <swu@nait.ca>
2022-09-06 14:51:05 -0600

    Added HockeyTeam

---
+++ cpsc1517-1221-a03-demos/OOPReview1/HockeyTeam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+    public class HockeyTeam
+    {
+        public enum HockeyConference { Eastern, Western }
+        public enum HockeyDivision { Metropolitan, Atlantic, Central, Pacific }
+
+        // Define data fields for storing data
+        private HockeyConference _conference;
+        private HockeyDivision _division;
+
+        // Define fully-implmented properties for the data fields
+        public HockeyConference Conference
+        {
+            get { return _conference; }
+        }
+        public HockeyDivision Division
+        {
+            get { return _division; }
+            set { _division = value; }  
+        }
+    }
+}
