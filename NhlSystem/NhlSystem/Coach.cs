7b1ec664cd705a4e1242587c30c0d9b17b6da633
Sam Wu <swu@nait.ca>
2022-09-16 11:49:46 -0600

    Added inheritance example

---
+++ NhlSystem/NhlSystem/Coach.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    // Define a class name Coach that inherits from the base class Person
+    public class Coach : Person
+    {
+        // Define a auto-implemented property for HireDate 
+        public DateTime HireDate { get; set; }
+
+        // Define constructor that passes FullName to the Person base class
+        public Coach(string fullName, DateTime hireDate) : base(fullName)
+        {
+            HireDate = hireDate;
+        }
+    }
+}
---
+++ NhlSystem/NhlSystem/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    // Define a class name Player that inherits from the Person base class
+    public class Player : Person
+    {
+        // Define auto-implemented properties for Position, Goals, Assists, PrimaryNo
+        public Position Position { get; set; }  
+        public int Goals { get; set; }
+        public int Assists { get; set; }
+        public int PrimaryNo { get; set; }
+
+        // Define a read-only computed property for Points
+        public int Points
+        {
+            get
+            {
+                return Goals + Assists;
+            }
+        }
+
+        // Define constructor that passes fullName to base class
+        public Player(string fullName, Position position, int primaryNo) : base(fullName)
+        {
+            Position = position;
+            PrimaryNo = primaryNo;
+            Goals = 0;
+            Assists = 0;
+        }
+        public Player(string fullName, Position position, int primaryNo, int goals, int assists) : base(fullName)
+        {
+            Position = position;
+            PrimaryNo = primaryNo;
+            Goals = goals;
+            Assists = assists;
+        }
+    }
+}
