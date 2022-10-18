6bde7ed7d200bb3d5b9d6768f9ff5d450193b809
Sam Wu <swu@nait.ca>
2022-10-18 14:32:28 -0600

    Completed Send Mail example

--- WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
@@ -7,6 +7,24 @@ namespace WestwindWebApp.Pages
 {
     public class SendMailModel : PageModel
     {
+        public void OnGet()
+        {
+            //var gmailUsername = Configuration["GmailCredentials:Username"];
+            //var gmailAppPassword = Configuration["GmailCredentials:Password"];
+            //FeedbackMessage = $"Gmail username = {gmailUsername} <br />";
+            //FeedbackMessage += $"Gmail app password = {gmailAppPassword} <br />";
+        }
+
+        private readonly IConfiguration Configuration;
+
+        public SendMailModel(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        [BindProperty]
+        public string FeedbackMessage { get; set; }
+
         [BindProperty]
         public string MailUsername { get; set; }
 
@@ -22,16 +40,20 @@ namespace WestwindWebApp.Pages
         [BindProperty]
         public string MailMessage { get; set; }
 
-        public void OnGet()
+        public void OnPostSendMail()
         {
-          
+            //FeedbackMessage = "<h2>Send Mail button clicked</h2>";
             var sendMailClient = new SmtpClient();
             sendMailClient.Host = "smtp.gmail.com";
             sendMailClient.Port = 587;
             sendMailClient.EnableSsl = true;
+
             var sendMailCredentials = new NetworkCredential();
+            MailUsername = Configuration["GmailCredentials:Username"];
+            MailAppPassword = Configuration["GmailCredentials:Password"];
             sendMailCredentials.UserName = MailUsername;
             sendMailCredentials.Password = MailAppPassword;
+
             sendMailClient.Credentials = sendMailCredentials;
 
             var sendFromAddress = new MailAddress(MailUsername);
@@ -40,8 +62,27 @@ namespace WestwindWebApp.Pages
             var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
             mailMessage.Subject = MailSubject;
             mailMessage.Body = MailMessage;
-            sendMailClient.Send(mailMessage);
 
+            try
+            {
+                sendMailClient.Send(mailMessage);
+                // Clear the form fields associated with the properties below
+                MailToAddress = "";
+                MailSubject = "";
+                MailMessage = "";
+                FeedbackMessage = "<div class='alert alert-primary'>Email Sent!</div>";
+            }
+            catch (Exception ex)
+            {
+                FeedbackMessage = $"<div class='alert alert-danger'>Error sending email {ex.Message}</div>";
+            }
         }
+
+        public void OnPostClearForm()
+        {
+            FeedbackMessage = "<h2>Clear Form button clicked</h2>";
+        }
+
+   
     }
 }
