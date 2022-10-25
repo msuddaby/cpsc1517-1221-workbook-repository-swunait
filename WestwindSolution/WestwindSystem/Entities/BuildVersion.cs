816099db7ea519bd1bc5b9b9c94aa1795ddf1440
Sam Wu <swu@nait.ca>
2022-10-25 14:48:10 -0600

    Added entity class for BuildVersion and Category

--- WestwindSolution/WestwindSystem/Entities/BuildVersion.cs
+++ WestwindSolution/WestwindSystem/Entities/BuildVersion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace WestwindSystem.Entities
 {
     public class BuildVersion
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
     }
 }
--- WestwindSolution/WestwindSystem/Entities/Category.cs
+++ WestwindSolution/WestwindSystem/Entities/Category.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WestwindSystem.Entities
 {
+    [Table(name:"Categories")]
     public class Category
     {
+        [Key]
+        [Column(name:"CategoryID")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "CategoryName is required")]
+        [MaxLength(15,ErrorMessage = "CategoryName cannot contain more than 15 characters")]
+        public string CategoryName { get; set; } = String.Empty;
+        [Column(TypeName = "ntext")]
+        public string? Description { get; set; }
+        [Column(TypeName = "varbinary")]
+        public byte[]? Picture { get; set; }
+        public string? PictureMimeType { get; set; }
 
     }
 }
