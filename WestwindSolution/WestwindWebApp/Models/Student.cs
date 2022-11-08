92add8d1d5e1099eba9934a7a244ebfbfca7127c
Sam Wu <swu@nait.ca>
2022-11-08 14:50:40 -0700

    Added project selecton.

---
+++ WestwindSolution/WestwindWebApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace WestwindWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public string FullName => $"{FirstName} {LastName}";
+        //public string FullName
+        //{
+        //    get 
+        //    { 
+        //        return $"{FirstName} {LastName}"; 
+        //    }
+        //}
+    }
+}
